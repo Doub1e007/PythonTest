aa230911253d5e1181c72a89a874c5e525194d34
Doub1e007 <3477848352@qq.com>
2025-04-07 20:32:08 +0800

    学生管理系统骨架

--- forward/StudentManagerSystem/managerSystem.py
+++ forward/StudentManagerSystem/managerSystem.py
@@ -6,30 +6,32 @@ class StudentManager(object):
     # 一、程序入口函数
     def run(self):
         # 1. 加载文件存储的数据
+        self.load_student()
         while True:
             # 2. 显示功能菜单
+            self.show_menu()
             # 3. 用户选择功能
             menu_num = int(input('请输入您需要的功能序号：'))
 
             # 4. 根据用户输入的序号执行不同的功能 -- 通过用户执行1 执行1添加
             if menu_num == 1:
                 # 添加学员
-                pass
+                self.add_student()
             elif menu_num == 2:
                 # 删除学员
-                pass
+                self.del_student()
             elif menu_num == 3:
                 # 修改学员
-                pass
+                self.modify_student()
             elif menu_num == 4:
                 # 查询学员
-                pass
+                self.search_student()
             elif menu_num == 5:
                 # 显示所有学员
-                pass
+                self.show_student()
             elif menu_num == 6:
                 # 保存学员信息
-                pass
+                self.save_student()
             elif menu_num == 7:
                 # 退出系统
                 break
@@ -47,10 +49,31 @@ class StudentManager(object):
         print('5. 显示所有学员信息')
         print('6. 保存学员信息')
         print('7. 退出系统')
+
     # 2.2 添加学员
+    def add_student(self):
+        print('添加学员')
+
     # 2.3 删除学员
+    def del_student(self):
+        print('删除学员')
+
     # 2.4 修改学员信息
+    def modify_student(self):
+        print('修改学员信息')
+
     # 2.5 查询学员信息
+    def search_student(self):
+        print('查询学员信息')
+
     # 2.6 显示所有学员信息
+    def show_student(self):
+        print('显示所有学员信息')
+
     # 2.7 保存学员信息
+    def save_student(self):
+        print('保存学员信息')
+
     # 2.8 加载学员信息
+    def load_student(self):
+        print('加载学员信息')
